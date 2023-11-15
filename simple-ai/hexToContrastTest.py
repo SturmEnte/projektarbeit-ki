152fd3b3d65a4c59b6cb63fb826b6545e7ddf81a
JUlian2k5_ <junobarwig@t-online.de>
2023-11-15 22:36:32 +0100

    Structured Code with addidionts needs reworking!

--- simple-ai/hexToContrastTest.py
+++ simple-ai/hexToContrastTest.py
@@ -1,11 +1,38 @@
-hexWert1 = input("Hintergrundfarbe zum Testen #")
+hexWert1 = input("Textfarbe zum Testen #")
+hexWert2 = input("Hintergrundfarbe zum Testen #")
 
-def hexToRgb1():
-    red1 = tuple(int(hexWert1[i:i+2], 16) for i in (0,))
-    print(f"Red1 = {red1}")
-    blue1 = tuple(int(hexWert1[i:i+2], 16) for i in (2,))
-    print(f"blue1 = {blue1}")
-    green1 = tuple(int(hexWert1[i:i+2], 16) for i in (4,))
-    print(f"green1 = {green1}")
+def hexToRgb(hexWert):
+    #roten Wert berechnen
+    red = int(hexWert[0:2], 16)
+    print(f"Red = {red}")
+    
+    #gruenen Wert berechnen
+    green = int(hexWert[2:4], 16)
+    print(f"green = {green}")
+    
+    #blauen Wert berechnen
+    blue = int(hexWert[4:], 16)
+    print(f"blue = {blue}")
+    
 
-hexToRgb1()
+    return red, green, blue
+
+def relativeLuminicance(red, green, blue):
+    l = 0.2126 * red + 0.7152 * green + 0.0722 * blue
+    print(l)
+    return l 
+
+def calculateContrast():
+    if l1 > l2:
+        contrast = (l1 + 0.05) / (l2 + 0.05)
+    else:
+        contrast = (l2 + 0.05) / (l1 + 0.05)
+    
+    print(f"Kontrast ist {contrast}:1")
+
+red, green, blue = hexToRgb(hexWert1)
+l1 = relativeLuminicance(red, green, blue)
+red, green, blue = hexToRgb(hexWert2)
+l2 = relativeLuminicance(red, green, blue)
+
+calculateContrast()
