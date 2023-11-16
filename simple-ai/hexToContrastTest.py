c936c7afb0a94388fa6dc5c729b529edf9f7c95d
Jb2k5 <julianbarwig2006@gmail.com>
2023-11-16 09:45:29 +0100

    Reworked to Black&White text. needs fine tuning

--- simple-ai/hexToContrastTest.py
+++ simple-ai/hexToContrastTest.py
@@ -1,10 +1,14 @@
-hexWert1 = input("Textfarbe zum Testen #")
-hexWert2 = input("Hintergrundfarbe zum Testen #")
+#By Julian Barwig 
+#Color Contrast Checker. 
+#Will now programm it to black and white text choosing relative to the backround.
+
+
+hexWertHintergund = input("Hintergrundfarbe zum Testen #")
 
 def hexToRgb(hexWert):
     #roten Wert berechnen
     red = int(hexWert[0:2], 16)
-    print(f"Red = {red}")
+    print(f"red = {red}")
     
     #gruenen Wert berechnen
     green = int(hexWert[2:4], 16)
@@ -17,22 +21,39 @@ def hexToRgb(hexWert):
 
     return red, green, blue
 
+#use formula to calculate the relative luminicance 
+#to later use the result to calculate the 
+# absolute contrast between two colors  
 def relativeLuminicance(red, green, blue):
     l = 0.2126 * red + 0.7152 * green + 0.0722 * blue
     print(l)
     return l 
 
-def calculateContrast():
-    if l1 > l2:
-        contrast = (l1 + 0.05) / (l2 + 0.05)
+def wOrBTextchoose(l):
+    if l < 140:
+        print("White text")
     else:
-        contrast = (l2 + 0.05) / (l1 + 0.05)
-    
-    print(f"Kontrast ist {contrast}:1")
+        print("Black text")
 
-red, green, blue = hexToRgb(hexWert1)
+#Check if what color has the higher number
+#and calculate the contrast:
+#
+#higherContrast / lowerContrast
+
+
+#def calculateContrast():
+#    if l1 > l2:
+#        contrast = (l1 + 0.05) / (l2 + 0.05)
+#    else:
+#        contrast = (l2 + 0.05) / (l1 + 0.05)
+#    
+#    print(f"Kontrast ist {contrast}")
+
+
+#red, green, blue = hexToRgb(hexWertText)
+#l1 = relativeLuminicance(red, green, blue)
+red, green, blue = hexToRgb(hexWertHintergund)
 l1 = relativeLuminicance(red, green, blue)
-red, green, blue = hexToRgb(hexWert2)
-l2 = relativeLuminicance(red, green, blue)
+wOrBTextchoose(l1)
 
-calculateContrast()
+#calculateContrast()
