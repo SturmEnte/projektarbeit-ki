9766f3cc5069d6b6eb1b061afd6bd736d61c64d0
Jb2k5_ <julianbarwig2006@gmail.com>
2023-11-17 14:17:08 +0100

    final stage. Gramatical errors still around...

--- simple-ai/hexToContrastTest.py
+++ simple-ai/hexToContrastTest.py
@@ -1,25 +1,24 @@
 #By Julian Barwig 
-#Color Contrast Checker. 
-#Will now programm it to black and white text choosing relative to the backround.
+#Color checker if White or Black text should be used. 
+#finished: Will now programm it to black and white text choosing relative to the backround.
 
 import random
 
-#hexWertHintergund = input("Hintergrundfarbe zum Testen #")
-
-coloriterations = 5
+colorIterations = input("How many trainingsdatas should be createt")
 red = []
 green = []
 blue = []
 
 result = []
 
-for i in range(coloriterations):
+for i in range(colorIterations):
    red.append(random.randint(0, 255))
    green.append(random.randint(0,255))
    blue.append(random.randint(0, 255))
 print(red, green, blue)
 
 
+#Is not used. Is a remnent to older days... aka when the programm was manuel
 def hexToRgb(hexWert):
     #roten Wert berechnen
     red = int(hexWert[0:2], 16)
@@ -33,7 +32,6 @@ def hexToRgb(hexWert):
     blue = int(hexWert[4:], 16)
     print(f"blue = {blue}")
     
-
     return red, green, blue
 
 #use formula to calculate the relative luminicance 
@@ -51,6 +49,6 @@ def wOrBTextChoose(l):
         print("Black text")
 
 #Calculate all the relative Luminicance and look if white or black text should be used
-for i in range(coloriterations):
+for i in range(colorIterations):
     l = relativeLuminicance(red[i], green[i], blue[i])
     wOrBTextChoose(l)
