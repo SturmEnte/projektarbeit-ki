1bc139ea00c70607249208158c2b6ab3257cf81a
SturmEnte <sturmente@proton.me>
2023-11-19 23:54:47 +0100

    Added jumping

--- complex-ai/jonas-test-01/main.py
+++ complex-ai/jonas-test-01/main.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 from player import Player
 
 # pygame setup
@@ -22,25 +23,34 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.jump()
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill((0, 128, 255))
 
     # Update
-
+    collided = False
     for player_collider in player_colliders:
         player_collider.update(player_collider.left - scroll_x, player_collider.top - scroll_y, player_collider.width, player_collider.height)
         
         if player.rect.colliderect(player_collider):
+            
+            while player_collider.top < player.rect.top + player.rect.height:
+                player.rect.update(player.rect.left, player.rect.top - 1, player.rect.width, player.rect.height)
+
             player.falling = False
             player.speed_y = 0
-    
+            collided = True
+
     player.update()
     
     # Render
-    player.draw()
     for player_collider in player_colliders:
         pygame.draw.rect(screen, (255, 255, 255), player_collider)
+
+    player.draw()
 
     # flip() the display to put your work on screen
     pygame.display.flip()
--- complex-ai/jonas-test-01/player.py
+++ complex-ai/jonas-test-01/player.py
@@ -1,6 +1,8 @@
 import pygame
 import time
-import math
+
+GRAVITY = 10
+JUMP_FORCE = 15
 
 class Player:
 
@@ -13,16 +15,24 @@ class Player:
 
         self.speed_x = 0
         self.speed_y = 0
+        self.speed_y_start = 0
         self.fall_start = time.time()
         self.falling = True
 
     def update(self):
-
         if self.falling:
-            # Fall down like in real live (10 m/s² == 100 pixels/s²)
-            self.speed_y = math.sqrt(2 * 100 * (time.time() - self.fall_start))
+            # Fall down like in real live (10 m/s² == 10 pixels/s²)
+            self.speed_y += GRAVITY * (time.time() - self.fall_start)
 
         self.rect.update(self.rect.left, self.rect.top + self.speed_y, self.rect.width, self.rect.height)
 
     def draw(self):
         pygame.draw.rect(self.screen, (255, 0, 255), self.rect)
+
+    def jump(self):
+        print("Jump")
+        self.speed_x = 200
+        if not self.falling:
+            self.speed_y = -JUMP_FORCE
+            self.falling = True
+            self.fall_start = time.time()
