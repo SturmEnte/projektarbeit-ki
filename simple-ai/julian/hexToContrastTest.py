5c261847c99f377945ef54b4b916c7382253007c
Jb2k5 <julianbarwig2006@gmail.com>
2023-11-26 21:50:20 +0100

    Outputs results into a textfile. Needs reworking!

--- simple-ai/julian/hexToContrastTest.py
+++ simple-ai/julian/hexToContrastTest.py
@@ -1,16 +1,18 @@
-#By Julian Barwig 
+#By Julian Barwig | Jb2k5
 #Color checker if White or Black text should be used. 
-#finished: Will now programm it to black and white text choosing relative to the backround.
 
 import random
 
-colorIterations = 0 #how many datas should be created
+colorIterations = 6 #how many files should be created
+
 red = []
 green = []
 blue = []
 
 result = []
+wOrB = []
 
+#create random rgb values
 for i in range(colorIterations):
    red.append(random.randint(0, 255))
    green.append(random.randint(0,255))
@@ -18,7 +20,7 @@ for i in range(colorIterations):
 print(red, green, blue)
 
 
-#Is not used. Is a remnent to older days... aka when the programm was manuel
+#hexToRgb Is not used! Is a remnent to older days... aka when the programm was manuel!
 def hexToRgb(hexWert):
     #roten Wert berechnen
     red = int(hexWert[0:2], 16)
@@ -35,20 +37,41 @@ def hexToRgb(hexWert):
     return red, green, blue
 
 #use formula to calculate the relative luminicance 
-#to later use the result to determin if white or black text should be used 
+#to later use the result to determin if white or black text should be used
+#Can break... Jk it can't
 def relativeLuminicance(red, green, blue):
     l = 0.2126 * red + 0.7152 * green + 0.0722 * blue
     print(l)
     result.append(l)
     return l 
 
+#check if black or white text should be used. 
+#wOrB = whiteOrBlack. in this case the color of the text. 
+#return 0 if white text should be used and 1(one) if black text should be used
 def wOrBTextChoose(l):
     if l < 140:
         print("White text")
+        a = 0
+        wOrB.append(a)
+        return a
     else:
         print("Black text")
+        a = 1
+        wOrB.append(a)
+        return a
+        
+#Write everything that could be usefull with for the ai and or the website in a textfile.
+#returns rgb-values, the relative-Luminicance and 0 || 1 if white or black text is used. 
 
-#Calculate all the relative Luminicance and look if white or black text should be used
+#needs to be reworked to be all in one textfile. For now it will work!
+def resultOutData(red, green, blue, l, a):
+        learningData = open(f"results{i}.txt","w")
+        learningData.write(f"{red, green, blue}" " | " f"{l}" " | " f"{a}")
+        learningData.close
+
+#Calculate all the relative Luminicance's and look if white or black text should be used. 
+#and also creates textfiles with resulst of one single(!) itteration. 
 for i in range(colorIterations):
     l = relativeLuminicance(red[i], green[i], blue[i])
     wOrBTextChoose(l)
+    resultOutData(red[i], green[i], blue[i], l, wOrB[i])
