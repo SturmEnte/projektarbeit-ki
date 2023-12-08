68d4e453156713ed46584b1274aceb8f120ce0b6
Jonas <sturmente@proton.me>
2023-12-08 10:52:34 +0100

    Ich weiß es auch nicht

--- simple-ai/jonas/main.js
+++ simple-ai/jonas/main.js
@@ -1,9 +1,11 @@
 const trainElement = document.getElementById("train");
 const colorElement = document.getElementById("color");
-const fontColorElement = document.getElementById("font-color");
+const writeWithWhiteElement = document.getElementById("font-color");
 const buttonElement = document.getElementById("button");
 
 let train = false;
+
+let trainingData = [];
 
 trainElement.oninput = () => {
 	train = trainElement.checked;
@@ -11,15 +13,23 @@ trainElement.oninput = () => {
 };
 
 function updatePage() {
-	fontColorElement.disabled = !train;
+	writeWithWhiteElement.disabled = !train;
 }
 
-buttonElement.oninput = () => {
+buttonElement.onclick = () => {
 	if (train) {
-		// Train ai
+		values = colorElement.value.split("#")[1];
+		console.log(values);
+		red = parseInt(Number(values.slice(0, 2)), 16);
+		green = parseInt(Number(values.slice(2, 4)), 16);
+		blue = parseInt(Number(values.slice(4, 6)), 16);
+		writeWithWhite = writeWithWhiteElement.checked;
+		trainingData.push([[red, green, blue], writeWithWhite]);
 	} else {
 		// Give output
 	}
 };
 
 updatePage();
+
+// AI
