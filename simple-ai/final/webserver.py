2e7b05b99a98715a27970e9fd1c29a322b5ae300
Unbekannt28 <mika@schiessler.net>
2023-12-12 19:08:01 +0100

    implemented flask webserver for color ai website

--- simple-ai/final/webserver.py
+++ simple-ai/final/webserver.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask import request
+from flask import Response
 from os import system
 
 IP = "127.0.0.1"
 
 app = Flask(__name__)
+
+def return_file(path):
+    content = ""
+    with open(path) as f:
+        content = f.read()
+    return content
 
 @app.route("/interface")
 def ai_request():
@@ -23,6 +30,29 @@ def ai_request():
         output = f.readline()
     
     return output
+
+# index page
+@app.route("/")
+def index():
+    return return_file("website/index.html")
+
+@app.route("/main.js")
+def index_js():
+    return Response(return_file("website/main.js"), mimetype="text/js")
+
+@app.route("/style.css")
+def index_css():
+    return Response(return_file("website/style.css"), mimetype="text/css")
+
+# about page
+@app.route("/about-site/")
+def about():
+    return return_file("website/about-site/index.html")
+
+@app.route("/about-site/style.css")
+def about_css():
+    return Response(return_file("website/about-site/style.css"), mimetype="text/css")
+
 
 if __name__ == "__main__":
     app.run(port=3000, host=IP)
