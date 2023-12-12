2bd12db5baf13abcae3c5d0f66fdb813a8b875fd
Jonas <sturmente@proton.me>
2023-12-12 17:49:46 +0100

    Deleted useless imports

--- simple-ai/final/src/main.rs
+++ simple-ai/final/src/main.rs
@@ -7,18 +7,6 @@ use neuroflow::activators::Type::Tanh;
 // use std::net::{TcpListener, SocketAddr};
 // use http::{Request, Response, StatusCode};
 
-// Webserver
-// use std::convert::Infallible;
-// use std::net::SocketAddr;
-
-// use http_body_util::Full;
-// use hyper::body::Bytes;
-// use hyper::server::conn::http1;
-// use hyper::service::service_fn;
-// use hyper::{Request, Response};
-// use hyper_util::rt::TokioIo;
-// use tokio::net::TcpListener;
-
 use std::{
     fs,
     io::{prelude::*, BufReader},
