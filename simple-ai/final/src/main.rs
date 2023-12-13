8d68eaa292398922d6150139cde78038a48b9dc7
Jonas <sturmente@proton.me>
2023-12-13 16:23:37 +0100

    Cleaned up rust code

--- simple-ai/final/src/main.rs
+++ simple-ai/final/src/main.rs
@@ -1,11 +1,7 @@
-// use std::io;
 use colore_ai_neuroflow::{self, commandinput, Commands, load_data};
 use neuroflow::{FeedForward, data, activators};
 use neuroflow::data::DataSet;
 use neuroflow::activators::Type::Tanh;
-
-// use std::net::{TcpListener, SocketAddr};
-// use http::{Request, Response, StatusCode};
 
 use std::{
     fs,
@@ -24,7 +20,6 @@ fn main() {
     
     nn = neuroflow::io::load("20231211_cai").unwrap();
 
-        // let i: Vec<f64> = vec![1.0, 1.0, 1.0];
         let mut i: Vec<f64> = Vec::new();
 
         let input = fs::read_to_string("input.txt").unwrap();
@@ -40,36 +35,3 @@ fn main() {
         fs::write("result.txt", format!("{}", result)).unwrap();
 
 }
-
-// pub fn run() -> io::Result<()> {
-
-//     // while exit == false {
-//     //     match commandinput() {
-//     //         Ok(Commands::Load(p)) => {
-//     //             nn = neuroflow::io::load(&p).unwrap();
-//     //         },
-//     //         Ok(Commands::Thinkm(i)) => {
-//     //             let results: &[f64] = nn.calc(&i);
-//     //             for result in results {
-//     //                 println!("{}", result);
-//     //             }
-//     //         },
-//     //         Ok(Commands::Exit) => exit = true,
-//     //         Ok(Commands::Save(p)) => { match neuroflow::io::save(&mut nn, &p) {
-//     //             Ok(_) => println!("saved"),
-//     //             Err(_) => eprintln!("Saving faild"),
-//     //         }},
-//     //         Ok(Commands::New(i)) => {
-//     //             nn = FeedForward::new(&i);
-//     //         },
-//     //         Ok(Commands::LoadData(p)) => data = load_data(p)?,
-//     //         Ok(Commands::Learn((lr, i))) => {
-//     //             nn.learning_rate(lr)
-//     //                 .train(&data, i)
-//     //         }
-//     //         Err(e) if e.kind() == io::ErrorKind::InvalidInput => eprintln!("{}", e),
-//     //         Err(e) => return Err(e)
-//     //     }
-//     // }
-//     return Ok(());
-// }
