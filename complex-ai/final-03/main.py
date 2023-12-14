b58f1889cb945c643d19270b762f6d999739597d
Unbekannt28 <mika@schiessler.net>
2023-12-14 14:48:02 +0100

    matched main to train structure

--- complex-ai/final-03/main.py
+++ complex-ai/final-03/main.py
@@ -4,10 +4,9 @@ from threading import Thread
 from time import sleep
 import tensorflow as tf
 import numpy as np
+from random import choice
 
 game = Game()
-game_thread = Thread(target=game.start)
-game_thread.start()
 
 screen_width = 1280
 screen_height = 720
@@ -21,15 +20,24 @@ tf.random.set_seed(SEED)
 
 model = tf.keras.models.load_model("ai_final")
 
-while True:
-    if game.initialized:
-        break
-    sleep(0.1)
+game = Game()
 
-while True:
-    if not game.initialized:
-        print(f"Distance: {game.player.traveled_distance}")
-        break
+
+game.moving_right = False
+
+def max_or_rand(array):
+    max = array[0]
+    equal_indicies = []
+    for i, element in enumerate(array):
+        if element == max:
+            equal_indicies.append(i)
+        elif element > max:
+            max = element
+            equal_indicies = [i]
+    return choice(equal_indicies)
+
+def callback(game):
+
     touching_distance = 0
     touching_y = 0
     nearest_distance = 0
@@ -52,29 +60,40 @@ while True:
     touching_y_normalized = touching_y / max_height_element
     player_y_normalized = player_y / max_height_element # can be grater than 1 (might cause problems)
     #print(f"Touching Distance: {touching_distance}\nTouching y: {touching_y}\nNearest Distance: {nearest_distance}\nNearest y: {nearest_y}")
+    #print(f"Normalized Values:\nTouching Distance: {touching_distance_normalized}\nTouching y: {touching_y_normalized}\nNearest Distance: {nearest_distance_normalized}\nNearest y: {nearest_y_normalized}")
     #print(f"Distance to Object 0: {game.game_objects[0].rect.left}")
-    #sleep(0.1)
+    print(f"Player y: {player_y}\nPlayer y normalized: {player_y_normalized}")
     
     predictions = model.predict([[touching_distance_normalized, touching_y_normalized, nearest_distance_normalized, nearest_y_normalized, player_y_normalized]]) # request prediction from model with normalized values
     #predictions = model.predict([[touching_distance, touching_y, nearest_distance, nearest_y, player_y]]) # request prediction from model with normalized values
-    result = np.argmax(predictions[0])
+    result = max_or_rand(predictions[0])
+
+    print(f"0: {predictions[0][0]}  1: {predictions[0][1]}  2: {predictions[0][2]}")
     
     # player should stand still
     if result == 0:
-        if moving_right:
+        if game.moving_right:
             game.player.move_right(False)
-            moving_right = False
+            game.moving_right = False
     
     # player should move to the right
     elif result == 1:
-        if not moving_right:
+        if not game.moving_right:
             game.player.move_right(True)
-            moving_right = True
+            game.moving_right = True
     
     # player should move to the right and jump
     elif result == 2:
-        if not moving_right:
+        if not game.moving_right:
             game.player.move_right(True)
-            moving_right = True
+            game.moving_right = True
         game.player.jump()
     #print(f"Prediction: 0: {round(predictions[0][0], 2)}; 1: {round(predictions[0][1], 2)}")
+    
+    if game.game_over:
+        print(f"Distance travelled: {game.player.traveled_distance}")
+    
+    #sleep(0.1)
+
+game.callback = callback
+game.start() # blocking call
